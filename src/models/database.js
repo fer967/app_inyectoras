b71a746b0ced862bbc6cb2f36caace0adb7057fd
Gabriel Fernando Correa <gabrielfcorrea3@gmail.com>
2025-03-30 20:04:26 -0300

    modifico batabase

--- src/models/database.js
+++ src/models/database.js
@@ -1,10 +1,33 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+
+/*const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres', 
     logging: false
+}); */
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // Ajustar según el entorno
+        }
+    }
 });
+
 
 const Inyectora = require('./inyectora.js')(sequelize, DataTypes);
 const Tecnico = require('./tecnico.js')(sequelize, DataTypes);
 
 module.exports = { sequelize, Inyectora, Tecnico };
+
+/*
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  logging: console.log, // Esto muestra los logs en la consola
+});
+
+*/
