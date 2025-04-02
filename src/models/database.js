748007d014fcb80ce8939ff559b2986bb92efa0f
Gabriel Fernando Correa <gabrielfcorrea3@gmail.com>
2025-04-02 15:47:06 -0300

    cambios en .env

--- src/models/database.js
+++ src/models/database.js
@@ -9,7 +9,7 @@ module.exports = { sequelize, Inyectora, Tecnico };
 
 
 
-// para reeplazar a DATABASE_URL
+// para reeplazar a DATABASE_URL     ¡¡¡¡ VER NO ANDA !!!!
 /*
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
@@ -22,3 +22,39 @@ DB_USER=postgres
 DB_PASSWORD=1986
 DB_HOST=localhost
 */
+
+
+// copia de seguridad 
+/*
+pg_dump -U usuario_local -h localhost -F c -b -v -f "archivo.dump" nombre_base_de_datos
+*/
+
+
+// configurar en supabase
+/*
+const { Sequelize } = require('sequelize');
+
+// Reemplaza estos valores con tus credenciales de Supabase
+const sequelize = new Sequelize({
+  host: 'db.supabase.co',
+  port: 5432,  // Puerto de PostgreSQL (por defecto)
+  username: 'usuario_supabase',
+  password: 'contraseña_supabase',
+  database: 'nombre_base_de_datos',
+  dialect: 'postgres',
+  logging: false, // Si quieres ver las consultas en consola, pon esto como 'true'
+});
+
+// Verificar la conexión
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexión a la base de datos establecida con éxito.');
+  })
+  .catch(err => {
+    console.error('Error al conectar a la base de datos:', err);
+  });
+*/
+
+
+// DATABASE_URL=postgresql://postgres:1986@localhost:5432/inyectoras_db
+  
